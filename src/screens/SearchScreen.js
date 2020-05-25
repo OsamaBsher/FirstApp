3522e5df891aa337a86e55906d47df714dd988b4
Osama <osama.bsher@student.manchester.ac.uk>
2020-05-25 09:28:51 +0100

    added suggestions on search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,74 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, Text, TextInput} from 'react-native';
+import {View, StyleSheet, Text, TextInput, FlatList, TouchableOpacity} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useHeaderHeight } from 'react-navigation-stack';
 import { Feather } from '@expo/vector-icons';
+import {ListItem} from 'react-native-elements';
 
 const SearchScreen = () => {
     const [search, setSearch] = useState('');
+
+    const cities = [
+        {
+            city: "طرابلس"
+        },
+        {
+            city: "بنغازي"
+        },
+        {
+            city: "مصراتة"
+        },
+        {
+            city: "زليتن"
+        },
+        {
+            city: "الزاوية"
+        },
+        {
+            city: "البيضاء"
+        },
+        {
+            city: "غريان"
+        },
+        {
+            city: "طبرق"
+        },
+        {
+            city: "صبراتة"
+        },
+        {
+            city: "سبها"
+        },
+        {
+            city: "الخمس"
+        },
+        {
+            city: "درنة"
+        },
+        {
+            city: "سرت"
+        },
+        {
+            city: "الجميل"
+        },
+        {
+            city: "الكفرة"
+        },
+        {
+            city: "المرج"
+        },
+        {
+            city: "يفرن"
+        },
+    ]
+
+    const [cityList, setCityList] = useState([]);
+
+    const setCities = (s) => {
+        if(s === ""){return setCityList([]);}
+        const list = cities.filter(city => {return city.city.search(new RegExp('^'+s)) >= 0});
+        return setCityList(list);
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
@@ -15,13 +78,25 @@ const SearchScreen = () => {
                     <TextInput 
                         style={styles.search}
                         placeholder="ابحث"
-                        onChangeText={(search) => setSearch(search)}
+                        onChangeText={(search) => {
+                            setSearch(search)
+                            setCities(search)
+                            }
+                        }
                         value={search}
                     />
                 </View>
             </View>
             <View style={styles.bottomContainer}>
-                <Text style={styles.text}>{search}</Text>
+                <FlatList
+                data={cityList}
+                keyExtractor={item => item.city}
+                renderItem={({item}) => {
+                    return <TouchableOpacity >
+                        <ListItem chevron title={item.city} />
+                    </TouchableOpacity>
+                }}
+                />
             </View>
         </SafeAreaView>
     );
